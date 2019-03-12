501a3e89ccaf1937f27ae023c68e7fd83d5de537
José María Martín Sánchez <jmmarsan@gmail.com>
2019-03-12 14:03:55 +0100

    React child components added

--- HelloWorldReact/Scripts/App.js
+++ HelloWorldReact/Scripts/App.js
@@ -1,9 +1,16 @@
+class Description extends React.Component {
+    render() {
+        return (<p> Esto es una prueba </p>);
+    }
+}
+
 class Hello extends React.Component {
   render() {
     return (<div>
               <h1 className="myDiv">Hola { this.props.name }</h1>
               <hr/>  
-              <p> Esto es una prueba </p>                                
+        <Description />
+        <Description />
             </div>);
   }
 }

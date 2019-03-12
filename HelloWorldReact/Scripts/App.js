531910c0964a0691cbe9b18d3965f8a0abb52dcc
José María Martín Sánchez <jmmarsan@gmail.com>
2019-03-12 16:25:15 +0100

    Show date added

--- HelloWorldReact/Scripts/App.js
+++ HelloWorldReact/Scripts/App.js
@@ -1,7 +1,17 @@
 class Description extends React.Component {
     render() {
-        return (<p> Esto es una prueba </p>);
+        return (<p> Esto es una prueba con una descripci&oacute;n bastante larga para ver c&oacute;mo se dispone utilizando las columnas y el espacio disponible</p>);
     }
+}
+
+class Grid extends React.Component {
+    render() {
+        return (<div className="grid2Columns">
+                    <Description />
+                    <Description />
+             </div>)
+   }
+   
 }
 
 class Hello extends React.Component {
@@ -9,8 +19,9 @@ class Hello extends React.Component {
     return (<div>
               <h1 className="myDiv">Hola { this.props.name }</h1>
               <hr/>  
-        <Description />
-        <Description />
+              <Grid />
+              <hr />
+        Hoy es {this.props.date}
             </div>);
   }
 }
